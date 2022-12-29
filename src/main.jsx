c6a56abb0f145c940d8c8753b71e233770a14335
vinayak <vinayak@gkmit.co>
2022-12-29 08:52:57 +0530

    Add home layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
+
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorBgContainer: '#E0E7FF',
+            fontSize: 16,
+          },
+        }}
+      >
+        <App />
+      </ConfigProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
