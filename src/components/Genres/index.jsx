0a04d52541451ddf5b7a1bcbac200d919afe36fc
vinayak <vinayak@gkmit.co>
2023-01-02 16:24:30 +0530

    Add genre delete feature

--- src/components/Genres/index.jsx
+++ src/components/Genres/index.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
-import { useQuery } from 'react-query';
+import { useQuery, useMutation } from 'react-query';
 import { useNavigate } from 'react-router-dom';
-import { Button, Table, Typography, Row, Col } from 'antd';
-import { PlusOutlined } from '@ant-design/icons';
+import { Button, Table, Typography, Row, Col, Space, Popconfirm } from 'antd';
+import { PlusOutlined, DeleteOutlined, EditOutlined } from '@ant-design/icons';
 const { Title, Text } = Typography;
 
 const URL = import.meta.env.VITE_URL;
@@ -14,17 +14,58 @@ function fetchGenres() {
     },
   });
 }
+function deleteGenre(id) {
+  return axios.delete(`${URL}/genres/${id}`, {
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_TOKEN}`,
+    },
+  });
+}
 
 export default function Genres() {
   const navigate = useNavigate();
-  const { data, isLoading, isError } = useQuery('genres', fetchGenres, {
+  const { data, isLoading, isError, refetch } = useQuery('genres', fetchGenres, {
     select: (data) => data.data.data.map((game) => game),
+  });
+
+  const { mutate } = useMutation(deleteGenre, {
+    onSuccess: () => {
+      refetch();
+    },
   });
 
   const columns = [
     { title: 'Id', dataIndex: 'id', key: 'id' },
     { title: 'Name', dataIndex: 'name', key: 'name' },
     { title: 'Slug', dataIndex: 'slug', key: 'slug' },
+    {
+      title: 'Action',
+      dataIndex: 'Action',
+      key: 'action',
+      render: (_, { id }) => {
+        return (
+          <Space>
+            <Popconfirm
+              title="Delete the entry"
+              description="Are you sure to delete this entry?"
+              onConfirm={() => {
+                mutate(id);
+              }}
+              okText="Yes"
+              cancelText="No"
+              placement="left"
+            >
+              <Button type="text" danger size="small">
+                <DeleteOutlined />
+              </Button>
+            </Popconfirm>
+            <Button type="text" size="small" onClick={() => navigate(`/game/${id}`)}>
+              <EditOutlined />
+            </Button>
+          </Space>
+        );
+      },
+    },
   ];
 
   const tableData = data?.map(({ id, attributes }) => {
